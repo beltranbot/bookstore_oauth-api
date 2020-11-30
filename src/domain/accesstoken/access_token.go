809739ac7e1032e8d0492b2159cfc93587f9f38a
carlos.beltran <beltranbot@gmail.com>
2020-11-29 21:05:31 -0500

    access_token type and tests

---
+++ src/domain/accesstoken/access_token.go
@@ -0,0 +1,27 @@
+package accesstoken
+
+import "time"
+
+const (
+	expirationTime = 24
+)
+
+// AccessToken struct
+type AccessToken struct {
+	AccessToken string `json:"access_token"`
+	UserID      int64  `json:"user_id"`
+	ClientID    int64  `json:"client_id"`
+	Expires     int64  `json:"expires"`
+}
+
+// GetNewAccessToken func
+func GetNewAccessToken() *AccessToken {
+	return &AccessToken{
+		Expires: time.Now().UTC().Add(expirationTime * time.Hour).Unix(),
+	}
+}
+
+// IsExpired func
+func (at AccessToken) IsExpired() bool {
+	return time.Unix(at.Expires, 0).Before(time.Now().UTC())
+}
